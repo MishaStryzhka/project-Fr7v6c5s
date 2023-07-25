4c076b476d5949a0e468815ca3dcebf5e1e3ac1a
MishaStryzhka <misha.strizhcko@gmail.com>
2023-07-25 16:32:40 +0000

    Deploying to gh-pages from @ MishaStryzhka/project-Fr7v6c5s@ab5a9ff12de4c0aee4331d1637f983c6101d4fee 🚀

--- static/js/475.9adab6dd.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[475],{3682:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)("p",{children:"NoticesPage"})}}}]);
-//# sourceMappingURL=475.9adab6dd.chunk.js.map
--- static/js/593.201eb33c.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[593],{8593:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)("p",{children:"AddPetPage"})}}}]);
-//# sourceMappingURL=593.201eb33c.chunk.js.map
